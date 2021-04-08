cae41d9e9b94ff9b17040a346a4b1a5d25dd0b26
Rishabh Singh <57044551+alphaion@users.noreply.github.com>
2021-04-08 23:03:13 +0530

    added variable.dart

---
+++ variable.dart
@@ -0,0 +1,7 @@
+/*Variable in dart */
+void main(){
+  var myName = "alphaion";
+  print(myName);
+}
+/*In dart, using semicolon ";" is mandatory at the end of statement. */
+/*Tip: You have to use the variable name in any way, otherwise it throws error. */
