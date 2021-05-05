83dba166847477836e1fc86ff297a2f8d29de7c4
alphaion <alphaion89@gmail.com>
2021-05-06 00:13:58 +0530

    checkNum

---
+++ Excercise/checkNum.dart
@@ -0,0 +1,15 @@
+// Even and Odd number checker function
+
+void main(){
+
+  checkNum(5);
+
+}
+
+void checkNum(int num){
+  if(num%2==0){
+    print("Even Number.");
+  }else{
+    print("Odd Number.");
+  }
+}
