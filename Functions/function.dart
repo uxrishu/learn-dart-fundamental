895c2ad5495d5b03f7994ef2c7308a9df2bceb51
alphaion <alphaion89@gmail.com>
2021-05-05 22:41:30 +0530

    Functions

---
+++ Functions/function.dart
@@ -0,0 +1,14 @@
+// Function: Collections of statments,
+// grouped together to perform an operation
+
+void main() {
+
+  findArea(3, 6);
+
+}
+void findArea(int length, int breadth){
+  
+  // Function works here
+  int area = (length*breadth);
+  print(area);
+}
