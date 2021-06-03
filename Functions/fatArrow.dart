559555e866dc5b2b648733ad0b42ce85a0c372cd
Rishabh Singh <alphaion89@gmail.com>
2021-06-04 00:03:44 +0530

    function added
    No extended description needed.

--- Functions/fatArrow.dart
+++ Functions/fatArrow.dart
@@ -5,8 +5,12 @@ void main(){
 
   int rectPerimeter = findPerimeter(5, 2);
   print("Perimeter: $rectPerimeter");
+  
+  areaOfCircle(4);
 }
 
 void findArea(int length, int breadth) => print("Area: ${length*breadth}");
 
 int findPerimeter(int length, int breadth) => 2*(length + breadth);
+
+int areaOfCircle(int radius) => print("Area of Circle: ${3.14*(radius**2)}");
